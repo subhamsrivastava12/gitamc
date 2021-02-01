bfb9067d999f4e211cc58a3f88a833073968df5c
Subham <subhamkumarr456@users.noreply.github.co>
2021-02-01 11:01:56 -0500

    Updates

---
+++ precache-manifest.9abd4b3e03ef4ef712e637bd8ce2e6e3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "667baaa82cb4c28a4f057240a48335a9",
+    "url": "/gitamc/index.html"
+  },
+  {
+    "revision": "68877a601589c1b81356",
+    "url": "/gitamc/static/css/2.b95e1924.chunk.css"
+  },
+  {
+    "revision": "0aa6fd262530d0d7e355",
+    "url": "/gitamc/static/css/main.0f758607.chunk.css"
+  },
+  {
+    "revision": "68877a601589c1b81356",
+    "url": "/gitamc/static/js/2.35f84f21.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/gitamc/static/js/2.35f84f21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aa6fd262530d0d7e355",
+    "url": "/gitamc/static/js/main.0e552ae9.chunk.js"
+  },
+  {
+    "revision": "9c481832f22f9416b6d9",
+    "url": "/gitamc/static/js/runtime-main.8cfa9324.js"
+  },
+  {
+    "revision": "b46cd2568b8aad1fda158ef8e72746ff",
+    "url": "/gitamc/static/media/Trophy.b46cd256.svg"
+  },
+  {
+    "revision": "65f54c25892bf0025a20208b3040d9c1",
+    "url": "/gitamc/static/media/logo.65f54c25.jpeg"
+  }
+]);
